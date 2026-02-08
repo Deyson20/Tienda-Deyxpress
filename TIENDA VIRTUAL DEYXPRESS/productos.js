cb1d48cc098074f1aeb2ae4307059b5c2ba9f770
Deyson20 <137962390+Deyson20@users.noreply.github.com>
2026-02-07 22:57:54 -0500

    Add files via upload

---
+++ TIENDA VIRTUAL DEYXPRESS/productos.js
@@ -0,0 +1,61 @@
+const productos = [
+  {
+    "id": 3,
+    "name": "Computador Didáctico Niños",
+    "price": 55000,
+    "category": "Tecnología",
+    "video": "https://www.youtube.com/embed/iqKu0HK7GmU",
+    "variants": [
+      "Azul"
+    ],
+    "images": [
+      "https://d39ru7awumhhs2.cloudfront.net/colombia/products/503271/1700144088WhatsApp%20Image%202023-11-16%20at%209.13.44%20AM.jpeg",
+      "https://d39ru7awumhhs2.cloudfront.net/colombia/products/503271/1700144088WhatsApp%20Image%202023-11-16%20at%209.13.52%20AM.jpeg",
+      "https://d39ru7awumhhs2.cloudfront.net/colombia/products/503271/1700144088WhatsApp%20Image%202023-11-16%20at%209.13.35%20AM.jpeg",
+      "https://d39ru7awumhhs2.cloudfront.net/colombia/products/503271/1700144088WhatsApp%20Image%202023-11-16%20at%209.13.44%20AM.jpeg"
+    ],
+    "description": "Este fabuloso juego didáctico cuenta con una infinidad de opciones para que sus niños aprendan y desarrollen nuevas habilidades de una forma dinámica."
+  },
+  {
+    "id": 1769365818400,
+    "name": "Pesa Maletas Bascula Digital Portatil 50 Kg Balanza Viajera",
+    "price": 18000,
+    "category": "Viajes",
+    "video": "https://www.youtube.com/embed/_yqp_E2hyRI",
+    "variants": [
+      "Negro",
+      "Azul",
+      "Naranja"
+    ],
+    "images": [
+      "https://d39ru7awumhhs2.cloudfront.net/colombia/products/1716886/1741820438Captura%20de%20pantalla%202025-03-12%20175934.png"
+    ],
+    "description": "Balanza electrónica digital de mano. Ideal para diversas situaciones y aplicaciones desde el peso de tus maletas antes de salir de viaje, en la oficina, hasta en el supermercado o plaza de mercado para tus compras, esta báscula electrónica portátil pesa de forma precisa lo que necesites. Con su pantalla LCD fácil de leer podrás ver el peso en cuestión de segundos y con su función tare y opciones de peso hace de esta bascula una de las más completas del mercado.\n\nLos viajeros frecuentes y turistas a veces terminan discutiendo con el personal del aeropuerto sobre exceso de equipaje y se enfrentan con el problema de dejar algunas de sus posesiones más preciadas o tener que pagar altas tarifas para equipaje extra. Para evitar situaciones agotadoras y molestas como ésta, es necesario medir tu equipaje antes de ir al aeropuerto. Esta balanza digital ligera y manejable puede medir hasta 50 kilogramos, por lo que es el dispositivo de peso perfecto para los viajeros.\n\n«« CARACTERÍSTICAS PRINCIPALES »»\nPuede medir hasta 50 kilogramos.\nPantalla LCD de 1.5 pulgadas de ahorro de energía.\nDiseño ligero, portátil y manejable.\nRequiere 2 baterías AAA para el funcionamiento. (Incluidas)\nPesa en kilos, libras y gramos\nPara pesos menores a 10 kg pesa de 5 en 5 gr y para pesos mayores a 10 kg. pesa de 10 en 10 gr.\n\nSE ENVIA EL COLOR DISPONIBLE AL MOMENTO DE LA COMPRA\n\n«« ELEMENTOS INCLUIDOS »»\nBascula\n\n«« DIMENSIONES »»\n21 cm de largo total (incluidos los ganchos)\n7 cm de ancho (parte más ancha)\n12 cm de largo (solo el cuerpo de la bascula)\nEn las imágenes puedes observar el detalle de las medidas"
+  },
+  {
+    "id": 1769567244589,
+    "name": "Car Bluetooth",
+    "price": 20000,
+    "category": "Tecnología",
+    "video": "https://www.youtube.com/embed/pWVKSIfYhfI",
+    "variants": [],
+    "images": [
+      "https://d39ru7awumhhs2.cloudfront.net/colombia/products/1862956/1751057905CAR%202.jpg",
+      "https://d39ru7awumhhs2.cloudfront.net/colombia/products/1862956/1751057905CAR%201.jpg"
+    ],
+    "description": "El Bluetooth en el contexto automotriz ha revolucionado la forma en que los conductores interactúan con sus vehículos y dispositivos móviles. La tecnología de conexión inalámbrica Bluetooth permite a los conductores sincronizar sus teléfonos inteligentes u otros dispositivos compatibles con el sistema de entretenimiento del automóvil, proporcionando una experiencia de conducción más segura, cómoda y conectada."
+  },
+  {
+    "id": 1770432079803,
+    "name": "Combo Nano Titaniumplanchapeineondu",
+    "price": 65900,
+    "category": "Tecnología",
+    "video": "",
+    "variants": [],
+    "images": [
+      "https://d39ru7awumhhs2.cloudfront.net/colombia/products/423735/16963563612.jpg",
+      "https://d39ru7awumhhs2.cloudfront.net/colombia/products/423735/16963563611.jpg"
+    ],
+    "description": "Combo Nano Titanium Plancha, Peine Y RizadorCon esta fabulosa plancha lograrás resultados con un 40% más de rapidez que una plancha convencional, así restarás tiempo arreglando tu cabello, no esperes más para comprar esta maravillosa Plancha Digital Profesional.Además llévate un magnifico Rizador Curl Perfect que crea rizos y ondas perfectas para tu cabello, diviértete creando nuevos peinados con el Rizador Curl Perfect , te verás hermosa en poco tiempo sin necesidad de estar largas y tediosas horas en un salón de belleza.El fabuloso peine profesional el cual te ayudará a sostener tu cabello mientras lo planchas para así evitar al máximo las quemaduras.Este peine profesional está diseñado para que el proceso de planchado sea más eficiente y así obtener peinados espectaculares y por su moderno material de fabricación no se daña con el calor y es muy resistente\n\n• Marca: Babyliss\n\n• Modelo: Titanium\n\n• Voltaje 110\n\n• Piezas del set: 3\n\n• Tipo de cabello: Quebradizo\n\n• Funciones: Planchar,Alisar, Peinar\n\n• Funciones adicionales: Encrespa\n\nr• Con regulador de temperatura: Sí\n\n• Temperatura mínima - Temperatura máxima: 110 °C - 230 °C\n\n• Materiales de las placas: Titanio\n\n• Es apta para cabello seco y húmedo: No\n\n• Temperatura máxima: 230 °C\n\nCon la Plancha En Nano Titanio Digital ahora podrás tener un cabello liso y brillante en muy poco tiempo, gracias a la tecnología de Placas en Titanio que esta magnífica plancha integra. Su temperatura es graduable y alcanza diferentes temperaturas.\n\nRizador que está fabricado en materiales de alta calidad y resistencia , posee un diseño moderno e innovador con un tamaño perfecto que permite llevarlo a cualquier lugar. Su mango es muy cómodo, adaptable a tu mano. Crea rizos y ondas perfectas para tu cabello.\n\nFabuloso peine profesional el cual te ayudará a sostener tu cabello mientras lo planchas para así evitar al máximo las quemaduras. Éste peine profesional ésta diseñado para que el proceso de planchado sea más eficiente y así obtener peinados espectaculares. Por su moderno material de fabricación no se daña con el calor y es muy resistente"
+  }
+];
